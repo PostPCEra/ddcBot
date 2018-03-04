db11374e4144c5f546802aca35153019b1bbedbd
PostPcEra <vision21c@gmail.com>
2018-03-04 01:50:56 -0800

    Update code_gems.py

--- kitchensink/code_gems.py
+++ kitchensink/code_gems.py
@@ -22,3 +22,43 @@ zp = zip(l1, l2)
 
 slist = sorted(zp, key=lambda p: p[1])  # sorting  based on the values of y in each (x,y) pair , for x sort use p[0]
 # [(3, 'a'), (9, 'b'), (2, 'c'), (24, 'd'), (1, 'e')]
+
+# ------------------- extract relationship -------------------------
+#
+# -------------------  *****************  ---------------------------
+def extract_relationship(inp, outp):
+
+    zp = zip(inp, outp)
+    zlist = sorted(zp, key=lambda p: p[0])  # sorting  based on the values of x in each (x,y) pair
+
+    two_or_more = [x for x in outp if outp.count(x) > 1]
+    category_list = set(two_or_more) # removes duplicates
+    print(category_list)
+    # https: // stackoverflow.com / questions / 473099 / check - if -a - given - key - already - exists - in -a - dictionary - and -increment - it
+    occr = { 'remaining' : [ ] }
+    for x in uniq:
+        occr[x] = [ ]
+
+    a = occr['remaining']
+    print(occr)
+
+    for pair in zlist:
+        key, value = pair
+        if value in category_list:
+            tmp = occr[value]
+            tmp.append(key)
+            occr[value] = tmp
+        else:
+            tmp = occr['remaining']
+            tmp.append(key)
+            occr['remaining'] = tmp
+
+    print(occr)
+# ------------------- main -------------------------
+def main():
+    input_l = list(range(1,17))
+    output_l = [1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz', 'buzz', 11, 'fizz', 13, 14 ]
+    o = extract_relationship(input_l, output_l)
+
+# ------------------- call main -------------------
+main()
