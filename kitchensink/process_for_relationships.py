20ee1e2e4ddcb1d856e4d2c34ecb4fe4b3ce2216
Subba Reddy <asubbareddy@gmail.com>
2018-03-06 13:17:09 -0800

    v 1.0 fizz buzz works

--- kitchensink/process_for_relationships.py
+++ kitchensink/process_for_relationships.py
@@ -121,10 +121,42 @@ def get_loop_code(varname, value, oper,input_symbol, output_symbol):
     code = "{} = [ ]\n".format(output_symbol) + \
            "{} = {} \n".format(varname, value) + \
            "for x in {}:\n\t".format(input_symbol) + \
-           "{}.append({} {} x)\n\n\t".format(output_symbol, varname, oper)
+           "{}.append({} {} x)\n\n".format(output_symbol, varname, oper)
+    return code
+
+def get_category_code(dict1, delta,  input_symbol, output_symbol):
+
+    from operator import itemgetter
+
+    # convert dictionary to tuple list, then sort. dictonary sorts are hard and unreliable .
+    tuple_lst = list(dict1.items())
+    print(tuple_lst)
+    tuple_lst.sort(key=itemgetter(0))
+    tuple_lst.sort(key=itemgetter(1), reverse=True)
+
+    code = "{} = [ ]\n".format(output_symbol) + \
+           "for n in {}:\n\t".format(input_symbol)
+
+    segment = "if n % {} == 0:\n\t\t".format(tuple_lst[0][1]) + \
+                "element = '{}'\n\t".format(tuple_lst[0][0])
+
+    for key, value in tuple_lst[1:]:
+        tmp = "elif n % {} == 0:\n\t\t".format(value) + \
+                "element = '{}'\n\t".format(key)
+        segment = segment + tmp
+
+    tmp2 = "" if delta == 0 else " + {}".format(delta)
+    tmp = "else:\n\t\t" + \
+          "element = n {}\n\n".format(tmp2) + \
+            "{}.append(element)\n\n\t".format(output_symbol)
+
+    code = code + segment + tmp
+    print(code)
     return code
 
 def generate_code(categories, cat_relationships):
+
+    input_symbol, output_symbol = 'a', 'b'
 
     length = len(categories)
     if length == 1:
@@ -134,9 +166,25 @@ def generate_code(categories, cat_relationships):
         else:
             varname, value, oper = ("ratio", output[0]/input[0], "*")
 
-        input_symbol, output_symbol = 'a', 'b'
         code = get_loop_code(varname, value, oper, input_symbol, output_symbol)
         #print(code)
+        return code
+
+    else:
+        eachcat_with_onevalue = {}  # init
+        for key, value in categories.items():
+            if key == CAT_TYPE_ALLOTHER:
+                input_l, output_l = value
+                delta = output_l[0] - input_l[0]
+                eachcat_with_onevalue[key] = delta
+            else:
+                eachcat_with_onevalue[key] = value[0]
+
+
+        delta = eachcat_with_onevalue.pop(CAT_TYPE_ALLOTHER, None) # save value and remove key
+        print(eachcat_with_onevalue)
+
+        code = get_category_code(eachcat_with_onevalue, delta,  input_symbol, output_symbol)
         return code
 
 # ------------------- process for relationship -------------------------
@@ -165,18 +213,19 @@ def process_for_relationships(input_l, output_l):
 # ------------------- main -------------------------
 #
 # ---------------------------------------------------
-
 def main():
 
     test_suite = [  ([1, 5, 8], [6, 10, 13]),
                     ([2, 6, 7], [10, 30, 35]),
-                    (list(range(1, 15)), [1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz', 'buzz', 11, 'fizz', 13, 14] )
-                 ]
+                    (list(range(1, 15)), [1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz', 'buzz', 11, 'fizz', 13, 14] )             ]
 
     for pair in test_suite:
         input, output = pair
         o = process_for_relationships(input, output)
 
 # ------------------- call main -------------------
-main()
+# invoke/call main() only when this stand alone file is executed as '$python <file.py>'
+# when this file is imported into another file, main() is NOT called
+if __name__ == '__main__':
+    main()
 
