6e6594c9645a69eb600829b16247d496b2832b22
Subba Reddy <asubbareddy@gmail.com>
2018-03-07 13:02:44 -0800

    Sorting and Grouping Nested Lists

--- kitchensink/code_gems.py
+++ kitchensink/code_gems.py
@@ -7,7 +7,14 @@ Created on Fri Feb 23 18:55:45 2018
 """
 # -------------------  Code Gems from web ---------------------------
 #
-# -------------------  *****************  ---------------------------
+# -------------------  *****************  -------------------------import timeit
+
+# https://docs.python.org/2/library/timeit.html
+import timeit
+timeit.timeit('"-".join(str(n) for n in range(100))', number=10000)
+timeit.timeit('"-".join([str(n) for n in range(100)])', number=10000)
+# 10000 loops, best of 3: 0.22 sec per loop ; # this is fastest execution time, that is lower time
+timeit.timeit('"-".join(map(str, range(100)))', number=10000)
 
 # https://stackoverflow.com/questions/9835762/find-and-list-duplicates-in-a-list
 lst = [1, 2, 3, 4, 4, 5, 5, 6, 1]
@@ -23,6 +30,38 @@ zp = zip(l1, l2)
 
 slist = sorted(zp, key=lambda p: p[1])  # sorting  based on the values of y in each (x,y) pair , for x sort use p[0]
 print(slist)  # [(3, 'a'), (9, 'b'), (2, 'c'), (24, 'd'), (1, 'e')]
+
+# https://stackoverflow.com/questions/14466068/sort-a-list-of-tuples-by-second-value-reverse-true-and-then-by-key-reverse-fal
+from operator import itemgetter
+d.sort(key=itemgetter(0))
+d.sort(key=itemgetter(1),reverse=True)
+
+
+# Sorting and Grouping Nested Lists in Python :
+# https://stackoverflow.com/questions/409370/sorting-and-grouping-nested-lists-in-python
+
+x = [
+ ['4', '21', '1', '14', '2008-10-24 15:42:58'],
+ ['3', '22', '4', '2somename', '2008-10-24 15:22:03'],
+ ['5', '21', '3', '19', '2008-10-24 15:45:45'],
+ ['6', '21', '1', '1somename', '2008-10-24 15:45:49'],
+ ['7', '22', '3', '2somename', '2008-10-24 15:45:51']
+]
+
+from operator import itemgetter
+x.sort(key=itemgetter(1))
+
+from itertools import groupby
+y = groupby(x, itemgetter(1))  # Now y is an iterator containing tuples of (element, item iterator).
+
+for elt, items in groupby(x, itemgetter(1)):
+    print(elt, items)
+    for i in items:
+        print(i)
+
+from pprint import pprint as pp
+pp([y for y in x if y[3] == '2somename'])
+
 
 # --------------  Global Variable ------------------------
 # https://stackoverflow.com/questions/423379/using-global-variables-in-a-function-other-than-the-one-that-created-them?rq=1
